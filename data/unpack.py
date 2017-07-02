2272e25134bb7a2695461acb0f904b59c33b9273
Sanat Sharma <sanatcolumban@gmail.com>
2017-07-01 21:02:46 -0700

    Added file to look at test data

---
+++ data/unpack.py
@@ -0,0 +1,13 @@
+import gzip
+import pickle
+import numpy
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+with gzip.open('mnist.pkl.gz', 'rb') as f:
+    train_set, valid_set, test_set = pickle.load(f)
+    train_x, train_y = train_set
+
+plt.imshow(train_x[1].reshape((28, 28)), cmap=cm.Greys_r)
+plt.show()
